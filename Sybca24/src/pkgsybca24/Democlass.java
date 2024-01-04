4ebd372cdb54fc32669fd58700f733673504077d
Ruksar Khatun <ruksarkhatun550@gmail.com>
2024-01-04 10:08:36 +0530

    first commit

---
+++ Sybca24/src/pkgsybca24/Democlass.java
@@ -0,0 +1,11 @@
+package pkgsybca24;
+
+public class Democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Github Hello World");
+	}
+
+}
