587a0ad94bcb8190c020dbf910e4335b2ee0ac0c
Ruksar Khatun <ruksarkhatun550@gmail.com>
2024-01-18 10:33:21 +0530

    commit

---
+++ Sybca24/src/pkgsybca24/Addnum.java
@@ -0,0 +1,16 @@
+package pkgsybca24;
+
+public class Addnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		int b = 3;
+		int sum = a+b;
+		int sub = a-b;
+		System.out.println("The sum is: "+sum);
+		System.out.println("The difference is: "+sub);
+	}
+
+}
